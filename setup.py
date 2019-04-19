019320e998985277e2ed0b87ec4a441242557227
Junhong Lin <junhonghust@gmail.com>
2019-04-19 17:05:05 +0800

    Update Repo

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import glob
 import os
-import shutil
-import sys
 import subprocess
+import sys
+import shutil
+
+board = os.environ['BOARD']
+board_folder = 'boards/{}/'.format(board)
 
 if os.geteuid() != 0:
     print("This program must be run as root. Aborting.")
     sys.exit(1)
 
-if os.environ['BOARD'] != 'Pynq-Z2':
+if board != 'Pynq-Z2':
     print("Only supported on a Pynq-Z2 Board")
     exit(1)
 
@@ -20,8 +24,7 @@ setup(
 	author = "Junhong Lin",
 	author_email = "junhonghust@gmail.com",
 
-	include_package_data = True,
-	packages = ['pynq_car'],
+	packages = find_packages(),
 	package_data = {
 	'' : ['*.bit','*.tcl','*.py','*.bin','*.txt', '*.cpp', '*.h', '*.sh'],
 	},
@@ -34,8 +37,11 @@ setup(
 if 'install' in sys.argv:
 	if os.path.isdir(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/pynq-car/"):
 		shutil.rmtree(os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/pynq-car/")
-	shutil.copytree("boards/Pynq-Z2/notebooks",os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/pynq-car/")
-	shutil.copytree("pynq_car/overlays/Robot",os.path.join('/usr/local/lib',os.environ['PYNQ_PYTHON'],'dist-packages/pynq/overlays/Robot')) #copy overlay
-	if os.path.isdir(os.path.join('/usr/local/lib',os.environ['PYNQ_PYTHON'],'dist-packages/pynq/lib/arduino/')):
-		shutil.rmtree(os.path.join('/usr/local/lib',os.environ['PYNQ_PYTHON'],'dist-packages/pynq/lib/arduino/'))
-	shutil.copytree("pynq_car/lib/arduino",os.path.join('/usr/local/lib',os.environ['PYNQ_PYTHON'],'dist-packages/pynq/lib/arduino')) #copy lib
+	shutil.copytree("notebooks",os.environ["PYNQ_JUPYTER_NOTEBOOKS"]+"/pynq-car/")
+
+	if os.path.isdir(os.path.join('/usr/local/lib',os.environ['PYNQ_PYTHON'],'dist-packages/pynq/overlays/Robot')):
+		shutil.rmtree(os.path.join('/usr/local/lib', os.environ['PYNQ_PYTHON'], 'dist-packages/pynq/overlays/Robot'))
+	shutil.copytree(os.path.join(board_folder, 'overlays/Robot'), os.path.join('/usr/local/lib', os.environ['PYNQ_PYTHON'], 'dist-packages/pynq/overlays/Robot')) #copy overlay
+
+	for f in os.listdir(os.path.join(board_folder, 'lib/arduino/')):
+		shutil.copyfile(os.path.join(board_folder, 'lib/arduino/', f) ,os.path.join('/usr/local/lib', os.environ['PYNQ_PYTHON'], 'dist-packages/pynq/lib/arduino', f)) #copy lib
